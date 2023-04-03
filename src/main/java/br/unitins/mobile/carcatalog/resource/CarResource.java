78bfa6f782583626579766fe171138adb54c2192
Luan Coêlho de Souza <luan.souza@tjto.jus.br>
2023-04-03 18:53:44 -0300

    fix: Corrigindo arquivos de resource com domínios errados.

--- src/main/java/br/unitins/mobile/carcatalog/resource/CarResource.java
+++ src/main/java/br/unitins/mobile/carcatalog/resource/CarResource.java
@@ -1,28 +1,31 @@
 package br.unitins.mobile.carcatalog.resource;
 
-import br.unitins.mobile.carcatalog.model.Brand;
-import br.unitins.mobile.carcatalog.service.BrandService;
+import br.unitins.mobile.carcatalog.model.Car;
+import br.unitins.mobile.carcatalog.service.CarService;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/brands")
+@Path("/cars")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class CarResource {
 
     @Inject
-    BrandService brandService;
+    CarService carService;
 
     @GET
     public Response getAll() {
-        List<Brand> brands = brandService.getAll();
-        return Response.ok(brands).build();
+        List<Car> cars = carService.getAll();
+        return Response.ok(cars).build();
+    }
+
+    @POST
+    public Response create(Car car) {
+        carService.create(car);
+        return Response.status(Response.Status.CREATED).build();
     }
 }
